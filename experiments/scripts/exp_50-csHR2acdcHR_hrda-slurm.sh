d1b75e5212c6fd522a4c949cc8c634af6f848be5
Christos Sakaridis <csakarid@vision.ee.ethz.ch>
2022-09-05 14:51:53 +0200

    Create script for Slurm experiment with HRDA baseline on Cityscapes -> ACDC

---
+++ experiments/scripts/exp_50-csHR2acdcHR_hrda-slurm.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Specify time limit.
+#SBATCH --time=120:00:00
+#
+# Specify number of CPU cores.
+#SBATCH -n 8
+#
+# Specify memory limit per CPU core.
+#SBATCH --mem-per-cpu=16384
+#
+# Specify disk limit on local scratch.
+#SBATCH --tmp=80000
+#
+# Specify GPU type and number of required GPUs.
+#SBATCH --gpus=rtx_3090:1
+#
+# Specify file for logging standard output.
+#SBATCH --output=../logs/exp_50-csHR2acdcHR_hrda-slurm-01.o
+#
+# Specify file for logging standard error.
+#SBATCH --error=../logs/exp_50-csHR2acdcHR_hrda-slurm-01.e
+#
+# Specify open mode for log files.
+#SBATCH --open-mode=append
+
+/bin/echo Starting on: `date`
+
+# Experiment ID.
+EXP_ID="50"
+
+# Specify directories.
+export TMPDIR="${TMPDIR}"
+export SOURCE_DIR="/cluster/home/csakarid/code/SysCV/DISS"
+export SOURCE_DATASET="cityscapes"
+export TARGET_DATASET="acdc"
+export DIR_SOURCE_DATASET="${TMPDIR}/${SOURCE_DATASET}"
+export DIR_TARGET_DATASET="${TMPDIR}/${TARGET_DATASET}"
+export TAR_SOURCE_DATASET="/cluster/work/cvl/csakarid/data/Cityscapes/Cityscapes.tar.gz"
+export TAR_TARGET_DATASET="/cluster/work/cvl/csakarid/data/ACDC/ACDC_splits.tar.gz"
+
+# Perform initialization operations for the experiment.
+cd ${SOURCE_DIR}
+source /cluster/home/csakarid/DISS/bin/activate
+./experiments/scripts/initialization.sh
+python tools/convert_datasets/cityscapes.py data/cityscapes --nproc 8
+
+# Run the experiment.
+python run_experiments.py --exp ${EXP_ID}
+
+# Perform finalization operations.
+./experiments/scripts/finalization.sh
+
+/bin/echo Finished on: `date`
+
