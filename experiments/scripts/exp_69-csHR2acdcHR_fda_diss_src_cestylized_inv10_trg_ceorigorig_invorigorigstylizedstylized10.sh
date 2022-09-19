e6eeb5be6e4cfb072ad7e93ca55945cad350a364
Christos Sakaridis <csakarid@vision.ee.ethz.ch>
2022-09-19 18:08:13 +0200

    Implement ablation study experiments with batch size of 2

---
+++ experiments/scripts/exp_69-csHR2acdcHR_fda_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Specify time limit.
+#BSUB -W 48:00
+#
+# Specify number of CPU cores.
+#BSUB -n 8
+#
+# Specify memory limit per CPU core.
+#BSUB -R "rusage[mem=8192]"
+#
+# Specify disk limit on local scratch.
+#BSUB -R "rusage[scratch=300000]"
+#
+# Specify number of required GPUs.
+#BSUB -R "rusage[ngpus_excl_p=1]"
+#
+# Specify GPU type and number of required GPUs.
+#BSUB -R "select[gpu_model0==TeslaV100_SXM2_32GB]"
+#
+# Specify file for logging standard output.
+#BSUB -o ../logs/exp_69-csHR2acdcHR_fda_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10.o
+#
+# Specify file for logging standard error.
+#BSUB -e ../logs/exp_69-csHR2acdcHR_fda_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10.e
+
+/bin/echo Starting on: `date`
+
+# Experiment ID.
+EXP_ID="69"
+
+# Specify directories.
+export TMPDIR="${TMPDIR}"
+export SOURCE_DIR="/cluster/home/csakarid/code/SysCV/DISS"
+export SOURCE_DATASET="cityscapes"
+export TARGET_DATASET="acdc"
+export DIR_SOURCE_DATASET="${TMPDIR}/${SOURCE_DATASET}"
+export DIR_TARGET_DATASET="${TMPDIR}/${TARGET_DATASET}"
+export TAR_SOURCE_DATASET="/cluster/work/cvl/csakarid/data/Cityscapes/Cityscapes.tar.gz"
+export TAR_TARGET_DATASET="/cluster/work/cvl/csakarid/data/ACDC/ACDC_splits.tar.gz"
+
+# Perform initialization operations for the experiment.
+cd ${SOURCE_DIR}
+source /cluster/home/csakarid/DISS/bin/activate
+./experiments/scripts/initialization.sh
+python tools/convert_datasets/cityscapes.py ${DIR_SOURCE_DATASET} --nproc 8
+
+# Run the experiment.
+python run_experiments.py --exp ${EXP_ID}
+
+# Deactivate virtual environment for DISS.
+deactivate
+
+/bin/echo Finished on: `date`
+
