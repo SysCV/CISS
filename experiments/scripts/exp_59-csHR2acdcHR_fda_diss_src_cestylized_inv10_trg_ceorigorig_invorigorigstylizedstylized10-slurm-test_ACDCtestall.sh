343311c47f1b368413a9c36e2a1a3ff498c4a2d8
Christos Sakaridis <csakarid@vision.ee.ethz.ch>
2022-10-17 17:35:43 +0200

    Implement scripts for testing and/or evaluation of various DISS models and baselines

---
+++ experiments/scripts/exp_59-csHR2acdcHR_fda_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10-slurm-test_ACDCtestall.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Specify time limit.
+#SBATCH --time=3:59:59
+#
+# Specify number of CPU cores.
+#SBATCH -n 8
+#
+# Specify memory limit per CPU core.
+#SBATCH --mem-per-cpu=8192
+#
+# Specify disk limit on local scratch.
+#SBATCH --tmp=80000
+#
+# Specify GPU type and number of required GPUs.
+#SBATCH --gpus=rtx_3090:1
+#
+# Specify file for logging standard output.
+#SBATCH --output=../logs/exp_59-csHR2acdcHR_fda_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10-slurm-test_ACDCtestall.o
+#
+# Specify file for logging standard error.
+#SBATCH --error=../logs/exp_59-csHR2acdcHR_fda_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10-slurm-test_ACDCtestall.e
+#
+# Specify open mode for log files.
+#SBATCH --open-mode=append
+
+/bin/echo Starting on: `date`
+
+# Experiment ID.
+EXP_ID="59"
+
+# Specify directories.
+export TMPDIR="${TMPDIR}"
+export SOURCE_DIR="/cluster/home/csakarid/code/SysCV/DISS"
+export TARGET_DATASET="acdc"
+export DIR_TARGET_DATASET="${TMPDIR}/${TARGET_DATASET}"
+export TAR_TARGET_DATASET="/cluster/work/cvl/csakarid/data/ACDC/ACDC_splits.tar.gz"
+export TEST_ROOT="/cluster/work/cvl/csakarid/results/DISS/local-exp59/220912_1705_csHR2acdcHR_1024x1024_dacs_a999_fdthings_diss_src_cestylized_inv10_trg_ceorigorig_invorigorigstylizedstylized10_rcs001-20_hrda1-512-01_daformer_sepaspp_sl_mitb5_poly10warm_s1_e2d22"
+export CONFIG_FILE="${TEST_ROOT}/*${TEST_ROOT: -1}.json"
+export CHECKPOINT_FILE="${TEST_ROOT}/latest.pth"
+export SHOW_DIR="${TEST_ROOT}/ACDC_test_all/preds"
+export TRAINIDS_DIR="${TEST_ROOT}/ACDC_test_all/labelTrainIds"
+
+# Perform initialization operations for the experiment.
+cd ${SOURCE_DIR}
+module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy pigz
+./experiments/scripts/initialization_test_torch_1_11.sh
+source /cluster/home/csakarid/DISS_torch_1_9/bin/activate
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
+
+# Run the experiment.
+echo 'Config File:' $CONFIG_FILE
+echo 'Checkpoint File:' $CHECKPOINT_FILE
+echo 'Predictions Output Directory:' $SHOW_DIR
+python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --data-root ${DIR_TARGET_DATASET} --test-set --format-only --eval-options imgfile_prefix=${TRAINIDS_DIR} to_label_id=False --show-dir ${SHOW_DIR} --opacity 1
+
+# Perform finalization operations.
+# ./experiments/scripts/finalization_test.sh
+deactivate
+
+/bin/echo Finished on: `date`
+
