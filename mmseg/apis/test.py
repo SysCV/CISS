a8fb4bc9824405c3ef3f00975bac84cff5361452
Christos Sakaridis <csakarid@vision.ee.ethz.ch>
2023-03-05 15:48:48 +0100

    Ensure test outputs are in lossless PNG format

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -82,7 +82,7 @@ def single_gpu_test(model,
                 img_show = mmcv.imresize(img_show, (ori_w, ori_h))
 
                 if out_dir:
-                    out_file = osp.join(out_dir, img_meta['ori_filename'])
+                    out_file = osp.join(out_dir, img_meta['ori_filename']).replace('.jpg', '.png')
                 else:
                     out_file = None
 
