7198db3ca0e405e90e1127dc6ce7e5dfb2491e27
Christos Sakaridis <csakarid@vision.ee.ethz.ch>
2023-05-30 04:50:07 +0200

    Update test bash script

--- test.sh
+++ test.sh
@@ -4,13 +4,15 @@ TEST_ROOT=$1
 CONFIG_FILE="${TEST_ROOT}/*${TEST_ROOT: -1}.json"
 CHECKPOINT_FILE="${TEST_ROOT}/latest.pth"
 SHOW_DIR="${TEST_ROOT}/preds"
+TRAINIDS_DIR="${TEST_ROOT}/labelTrainIds"
+
+source ~/venv/CISS_test/bin/activate
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
+
 echo 'Config File:' $CONFIG_FILE
 echo 'Checkpoint File:' $CHECKPOINT_FILE
 echo 'Predictions Output Directory:' $SHOW_DIR
-python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --eval mIoU --show-dir ${SHOW_DIR} --opacity 1
-
-# Uncomment the following lines to visualize the LR predictions,
-# HR predictions, or scale attentions of HRDA:
-#python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --show-dir ${SHOW_DIR}_LR --opacity 1 --hrda-out LR
-#python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --show-dir ${SHOW_DIR}_HR --opacity 1 --hrda-out HR
-#python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --show-dir ${SHOW_DIR}_ATT --opacity 1 --hrda-out ATT
+# Inference.
+python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --format-only --eval-options imgfile_prefix=${TRAINIDS_DIR} to_label_id=False --show-dir ${SHOW_DIR} --opacity 1
+# Evaluation.
+python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --eval mIoU
